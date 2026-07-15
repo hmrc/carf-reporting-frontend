209a8e5d157c0b3fa6cca8df877cc0733fe5a31a
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2026-07-15 15:45:05 +0100

    CARF-570 Remove forms

--- .g8/multipleQuestionsPage/app/models/$section$/$className$.scala
+++
@@ -1,9 +0,0 @@
-package models.$section$
-
-import play.api.libs.json._
-
-case class $className$ ($field1Name$: String, $field2Name$: String)
-
-object $className$ {
-  implicit val format = Json.format[$className$]
-}
