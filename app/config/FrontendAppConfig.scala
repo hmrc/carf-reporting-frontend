b7e299f237c62f5c90ccba1ff0f22bfb0341d45a
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-07-24 11:49:22 +0100

    management home page url

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -31,20 +31,17 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
 
   val carfReportingBaseUrl: String = s"$carfReportingHost/carf-reporting"
 
-  private val carfManagementHost: String  = servicesConfig.baseUrl("carf-management")
-  val manageCryptoassetReportsUrl: String = s"$carfManagementHost/manage-cryptoasset-reports"
-
   private val contactHost                  = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "carf-reporting-frontend"
 
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${host + request.uri}"
 
-  val loginUrl: String         = configuration.get[String]("urls.login")
-  val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
-  val signOutUrl: String       = configuration.get[String]("urls.signOut")
-  val registrationUrl: String  = configuration.get[String]("urls.registration")
-
+  val loginUrl: String                  = configuration.get[String]("urls.login")
+  val loginContinueUrl: String          = configuration.get[String]("urls.loginContinue")
+  val signOutUrl: String                = configuration.get[String]("urls.signOut")
+  val registrationUrl: String           = configuration.get[String]("urls.registration")
+  val managementUrl: String             = configuration.get[String]("urls.managementUrl")
   private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
   val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/carf-reporting-frontend"
 
