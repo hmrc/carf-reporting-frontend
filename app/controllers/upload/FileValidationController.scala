17da7b358a20e829cf0001d2182966c799d11137
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-07-27 15:54:04 +0100

    controller and view

---
+++ app/controllers/upload/FileValidationController.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.upload
+
+import connectors.RcaspRegistrationConnector
+import controllers.actions._
+import pages.SendingEntityInPage
+import play.api.Logging
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+/*
+ * TODO: Remove/replace once real schema validation/data extraction is wired up (CARF-596).
+ *
+ * This controller is a test-only stub (see conf/testOnlyDoNotUseInAppConf.routes) that
+ * simulates the SendingEntityIN value that will eventually be returned from the backend
+ * after successful schema validation of the uploaded file. Test the three journeys for this
+ * ticket via the sendingEntityIn query parameter:
+ *   - sendingEntityIn=<a real RCASPID for the logged-in carfId>        -> placeholder "check your file details" page
+ *   - sendingEntityIn=<any non-matching value>                         -> /problem/rcasp-not-matching
+ *   - leave out the query parameter entirely                           -> Journey Recovery page
+ */
+class FileValidationController @Inject() (
+    identify: IdentifierAction,
+    getData: DataRetrievalAction,
+    requireData: DataRequiredAction,
+    sessionRepository: SessionRepository,
+    rcaspRegistrationConnector: RcaspRegistrationConnector,
+    val controllerComponents: MessagesControllerComponents
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with Logging {
+
+  def onPageLoad(sendingEntityIn: Option[String]): Action[AnyContent] =
+    (identify() andThen getData() andThen requireData).async { implicit request =>
+      sendingEntityIn match {
+        case None =>
+          logger.warn("[FileValidationController][onPageLoad] Unable to extract SendingEntityIN from file")
+          Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+
+        case Some(value) =>
+          rcaspRegistrationConnector.viewRcasps(request.carfId).value.flatMap {
+            case Left(error) =>
+              logger.warn(s"[FileValidationController][onPageLoad] Error calling viewRcasps: $error")
+              Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad()))
+
+            case Right(rcasps) =>
+              for {
+                updatedAnswers <- Future.fromTry(request.userAnswers.set(SendingEntityInPage, value))
+                _              <- sessionRepository.set(updatedAnswers)
+              } yield
+                if (rcasps.exists(_.RCASPID.equalsIgnoreCase(value))) {
+                  Redirect(
+                    controllers.routes.PlaceholderController.onPageLoad(
+                      "Check your file details page: to be built"
+                    )
+                  )
+                } else {
+                  Redirect(controllers.problem.routes.RcaspNotMatchingController.onPageLoad())
+                }
+          }
+      }
+    }
+}
