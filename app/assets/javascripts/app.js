27cdd6f62d669e7cd7d4598543c7527e34dd7649
May Kenworthy <247179073+maykenworthy@users.noreply.github.com>
2026-07-29 11:32:59 +0100

    CARF-393: File Upload - Upload an XML file (#5)
    * CARF-574: Added auth actions and relevant tests and usages
    
    * CARF-574: Removed testOnly view, Added IdentifierActionSpec and ran scalafmt
    
    * CARF-393: Latest changes for implementing spinner to page (BROKEN)
    
    * CARF-393: Slight changes (still broken)
    
    * CARF-393: Fixed spinner issues and added errors to page
    
    * CARF-393: Added some tests and got page working (tests fail)
    
    * CARF-393: Added csrf token to unit tests
    
    * CARF-393: Fixed javascript ordering and updated csrf token accordingly
    
    * CARF-393: Removed unused connector and added model tests
    
    * CARF-393: Re-added csrf token
    
    * CARF-393: Minor changes, removal of unused items, fixed regex
    
    * CARF-393: Updated regex
    
    * CARF-393: Replaced placeholder
    
    * CARF-393: Replaced label with fluency for consistency
    
    * CARF-393: compile format
    
    * CARF-393: replaced file upload component label with hint

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+// prevent resubmit warning
+if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
+    window.history.replaceState(null, null, window.location.href);
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    // handle back click
+    var backLink = document.querySelector('.govuk-back-link');
+    if (backLink !== null) {
+        backLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            window.history.back();
+        });
+    }
 
     // handle print click
     var printLink = document.querySelector('.print-link');
---
+++ app/controllers/upload/UploadXmlController.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.upload
+
+import config.FrontendAppConfig
+import controllers.actions.*
+import forms.UploadXmlFormProvider
+import models.ErrorCode.{InvalidArgument, OctetStream, VirusFile}
+import models.InvalidArgumentErrorMessage.{DisallowedCharacters, FileIsEmpty, InvalidFileNameLength, TypeMismatch}
+import models.upscan.{Reference, UpscanInitiateResponse}
+import models.{ErrorCode, InvalidArgumentErrorMessage}
+import org.apache.pekko
+import org.apache.pekko.actor.ActorSystem
+import play.api.Logging
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.upload.UploadXmlView
+
+import javax.inject.Inject
+import scala.concurrent.Future
+
+class UploadXmlController @Inject() (
+    override val messagesApi: MessagesApi,
+    identify: IdentifierAction,
+    getData: DataRetrievalAction,
+    requireData: DataRequiredAction,
+    actorSystem: ActorSystem,
+    config: FrontendAppConfig,
+    formProvider: UploadXmlFormProvider,
+    val controllerComponents: MessagesControllerComponents,
+    view: UploadXmlView
+) extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  // TODO: Remove when implementing Upscan functionality (CARF-578, CARF-579)
+  val upscanInitiateResponse = UpscanInitiateResponse(
+    fileReference = Reference("abc"),
+    postTarget = "http://localhost:17004/send-a-cryptoasset-report/report/upload-file",
+    formFields = Map.empty
+  )
+
+  val form: Form[String] = formProvider()
+
+  def onPageLoad(): Action[AnyContent] = (identify() andThen getData()) { implicit request =>
+    Ok(view(form, upscanInitiateResponse))
+  }
+
+  // TODO: Update when implementing Upscan functionality (CARF-578, CARF-579)
+  def onSubmit(): Action[AnyContent] = (identify() andThen getData()) { implicit request =>
+    Thread.sleep(1000)
+    Redirect(
+      controllers.routes.PlaceholderController.onPageLoad(
+        "Should redirect based on upscan checks: CARF-578, CARF-579"
+      )
+    )
+  }
+
+  def showError(errorCode: String, errorMessage: String, errorRequestId: String): Action[AnyContent] =
+    (identify() andThen getData()).async { implicit request =>
+
+      val formWithErrors: Form[String] = ErrorCode.fromCode(errorCode) match {
+        case Some(ErrorCode.EntityTooLarge)      => form.withError("file-upload", "uploadXml.error.file.size.large")
+        case Some(VirusFile)                     => form.withError("file-upload", "uploadXml.error.file.content.virus")
+        case Some(InvalidArgument | OctetStream) =>
+          InvalidArgumentErrorMessage.fromMessage(errorMessage) match {
+            case Some(InvalidFileNameLength) => form.withError("file-upload", "uploadXml.error.file.name.length")
+            case Some(DisallowedCharacters)  =>
+              form.withError("file-upload", "uploadXml.error.file.name.disallowed.characters")
+            case Some(TypeMismatch)          => form.withError("file-upload", "uploadXml.error.file.type.invalid")
+            case Some(FileIsEmpty)           => form.withError("file-upload", "uploadXml.error.file.content.empty")
+            case None                        => form.withError("file-upload", "uploadXml.error.file.select")
+          }
+        case _                                   =>
+          logger.warn(s"Upscan error $errorCode: $errorMessage, requestId is $errorRequestId")
+          form.withError("file-upload", "uploadXml.error.file.content.unknown")
+      }
+      Future.successful(Ok(view(formWithErrors, upscanInitiateResponse)))
+    }
+}
