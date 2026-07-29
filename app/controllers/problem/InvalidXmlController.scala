4d52fafc4b4ec8a65fee72d30f938bbcd8be2951
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-07-29 14:01:08 +0100

    comment update

--- app/controllers/problem/InvalidXmlController.scala
+++ app/controllers/problem/InvalidXmlController.scala
@@ -31,7 +31,7 @@ class InvalidXmlController @Inject() (
 ) extends FrontendBaseController
     with I18nSupport {
 
-  // TODO: replace hardcoded filename with the actual uploaded filename from UserAnswers once available (CARF-581)
+  // TODO: replace hardcoded filename with the actual uploaded filename from UserAnswers once available (CARF-596)
   private val fileName: String = "filename.xml"
 
   def onPageLoad: Action[AnyContent] = identify() { implicit request =>
