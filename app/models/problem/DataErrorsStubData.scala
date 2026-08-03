6fcfc4c4a80a60e32c2182f3a1b3f93d3d4eadad
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-08-03 16:46:27 +0100

    match prototype

--- app/models/problem/DataErrorsStubData.scala
+++ app/models/problem/DataErrorsStubData.scala
@@ -26,7 +26,7 @@ object DataErrorsStubData {
     SchemaError(
       10,
       Html(
-        """<p class="govuk-body govuk-!-margin-bottom-1">MessageRefId element must be from 26 to 100 characters. It must also match the file name and include the following in the order referenced:</p>
+        """<p class="govuk-!-margin-bottom-1">MessageRefId element must be from 26 to 100 characters. It must also match the file name and include the following in the order referenced:</p>
           |<ul class="govuk-list govuk-list--bullet">
           |  <li>&lsquo;GB&rsquo;</li>
           |  <li>the same value as the year in the MessageSpec ReportingPeriod in the format &lsquo;YYYY&rsquo;</li>
@@ -36,20 +36,20 @@ object DataErrorsStubData {
           |  <li>a hyphen (-)</li>
           |  <li>1 to 75 characters of your choice to make the ID unique</li>
           |</ul>
-          |<p class="govuk-body govuk-!-margin-bottom-0">MessageRefId must also not include less than signs (<), greater than signs (>), colons (:), straight double quotes ("), apostrophes ('), ampersands (&amp;), forward slashes (/), backslashes (\), vertical bars (|), question marks (?) or asterisks (*).</p>
+          |<p class="govuk-!-margin-bottom-0">MessageRefId must also not include less than signs (<), greater than signs (>), colons (:), straight double quotes ("), apostrophes ('), ampersands (&amp;), forward slashes (/), backslashes (\), vertical bars (|), question marks (?) or asterisks (*).</p>
           |""".stripMargin
       )
     ),
     SchemaError(
       12,
       Html(
-        """<p class="govuk-body govuk-!-margin-bottom-1">ReportingPeriod value must:</p>
+        """<p class="govuk-!-margin-bottom-1">ReportingPeriod value must:</p>
           |<ul class="govuk-list govuk-list--bullet">
           |  <li>be in the format YYYY-MM-DD</li>
           |  <li>be between 2026 and the end of the current year</li>
           |  <li>include 31 as the day and 12 as the month</li>
           |</ul>
-          |<p class="govuk-body govuk-!-margin-bottom-0">For example, 2026-12-31.</p>
+          |<p class="govuk-!-margin-bottom-0">For example, 2026-12-31.</p>
           |""".stripMargin
       )
     ),
@@ -59,13 +59,13 @@ object DataErrorsStubData {
     SchemaError(
       41,
       Html(
-        """<p class="govuk-body govuk-!-margin-bottom-1">DocRefId element must be from 28 to 164 characters and include the following in the order referenced:</p>
+        """<p class="govuk-!-margin-bottom-1">DocRefId element must be from 28 to 164 characters and include the following in the order referenced:</p>
           |<ul class="govuk-list govuk-list--bullet">
           |  <li>the same value as the MessageRefId for this submission</li>
           |  <li>a hyphen (-)</li>
           |  <li>1 to 63 characters of your choice to make the ID unique</li>
           |</ul>
-          |<p class="govuk-body govuk-!-margin-bottom-0">For an OECD0 file, the DocRefId must match the previous submission.</p>
+          |<p class="govuk-!-margin-bottom-0">For an OECD0 file, the DocRefId must match the previous submission.</p>
           |""".stripMargin
       )
     ),
