d9a47765423d3f8c86fa812df007b6fd965826a8
Aisha Bi <43678132+AishaBi@users.noreply.github.com>
2026-09-14 10:08:20 +0100

    CARF-615: File Upload - /results-of-automatic-checks creation and stubs (#24)
    * CARF-615: File Upload - /results-of-automatic-checks creation and stubs
    
    * .
    
    * compile errors fix
    
    * table fix
    
    * matthew table fix
    
    Co-Authored-By: Matthew Goh <186605436+matthew-goh@users.noreply.github.com>
    
    * links fix
    
    Co-Authored-By: Matthew Goh <186605436+matthew-goh@users.noreply.github.com>
    
    * unit test
    
    * update url
    
    * url update test
    
    * pr comments
    
    * test fix
    
    * remove extend Logging as using logwarn
    
    * swap hrefs
    
    * rename dir
    
    * stud data fix
    
    * datetime
    
    * time and controller fix
    
    * .
    
    * time fix
    
    * remove logging
    
    * time test added
    
    * addd upload id after rebase
    
    * scrolling fix
    
    * .
    
    ---------
    
    Co-authored-by: Matthew Goh <186605436+matthew-goh@users.noreply.github.com>

---
+++ app/models/fileSubmission/ResultOfAutomaticChecksStubData.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.fileSubmission
+
+import java.time.{LocalDateTime, ZoneOffset}
+
+object ResultOfAutomaticChecksStubData {
+
+  private val abcBank: String   = "ABC Bank plc"
+  private val myCompany: String = "My Company Ltd"
+
+  val passedFile: SlicedCachedFileDetails = SlicedCachedFileDetails(
+    abcBank,
+    "GB2026GB-CAR1234567892_Valid",
+    "upload-id-passed",
+    LocalDateTime.now(ZoneOffset.UTC),
+    FileStatus.Passed
+  )
+
+  val failedRulesFile: SlicedCachedFileDetails = SlicedCachedFileDetails(
+    abcBank,
+    "GB2026GB-CAR1234567892_BR-invalid",
+    "upload-id-failed",
+    LocalDateTime.now(ZoneOffset.UTC).minusMinutes(5),
+    FileStatus.Failed
+  )
+
+  val pendingFile: SlicedCachedFileDetails = SlicedCachedFileDetails(
+    abcBank,
+    "GB2026GB-CAR1234567892_CADX-down",
+    "upload-id-pending",
+    LocalDateTime.now(ZoneOffset.UTC).minusMinutes(10),
+    FileStatus.Pending
+  )
+
+  val unexpectedErrorFile: SlicedCachedFileDetails = SlicedCachedFileDetails(
+    myCompany,
+    "GB2026GB-CAR1234567891_SDES-error",
+    "upload-id-unexpected-error",
+    LocalDateTime.of(2023, 7, 22, 14, 10),
+    FileStatus.UnexpectedError
+  )
+
+  val virusFoundFile: SlicedCachedFileDetails = SlicedCachedFileDetails(
+    myCompany,
+    "GB2026GB-CAR1234567891_SDES-virus",
+    "upload-id-virus-found",
+    LocalDateTime.of(2023, 7, 23, 12, 0),
+    FileStatus.VirusFound
+  )
+
+  val unprocessableErrorFile: SlicedCachedFileDetails = SlicedCachedFileDetails(
+    myCompany,
+    "GB2026GB-CAR1234567892_CADX-error",
+    "upload-id-unprocessable-error",
+    LocalDateTime.of(2023, 7, 24, 8, 0),
+    FileStatus.UnprocessableErrorFile
+  )
+
+  val allStatuses: Seq[SlicedCachedFileDetails] =
+    Seq(unprocessableErrorFile, virusFoundFile, unexpectedErrorFile, pendingFile, failedRulesFile, passedFile)
+}
---
+++ test/controllers/submission/ResultOfAutomaticChecksControllerSpec.scala
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.submission
+
+import base.SpecBase
+import config.FrontendAppConfig
+import models.errors.ApiError.InternalServerError
+import models.fileSubmission.{ResultOfAutomaticChecksStubData, SlicedCachedFileDetails}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.*
+import play.api.inject.bind
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import services.AutomaticChecksStubService
+import types.ResultT
+import views.html.submission.ResultOfAutomaticChecksView
+
+import java.time.LocalDateTime
+
+class ResultOfAutomaticChecksControllerSpec extends SpecBase {
+
+  private val mockService: AutomaticChecksStubService = mock[AutomaticChecksStubService]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockService)
+  }
+
+  "ResultOfAutomaticChecks Controller" - {
+
+    "must return OK and the correct view when submissions are found" in {
+
+      when(mockService.getSubmissions(any())).thenReturn(ResultT.fromValue(ResultOfAutomaticChecksStubData.allStatuses))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[AutomaticChecksStubService].toInstance(mockService))
+        .build()
+
+      running(application) {
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(GET, routes.ResultOfAutomaticChecksController.onPageLoad().url)
+
+        val result = route(application, request).value
+        val view   = application.injector.instanceOf[ResultOfAutomaticChecksView]
+
+        val expectedSorted =
+          ResultOfAutomaticChecksStubData.allStatuses.sortBy(_.dateSubmitted)(Ordering[LocalDateTime].reverse)
+
+        status(result)          mustEqual OK
+        contentAsString(result) mustEqual
+          view(expectedSorted, appConfig.managementUrl)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery when the submissions list is empty" in {
+
+      when(mockService.getSubmissions(any()))
+        .thenReturn(ResultT.fromValue(Seq.empty[SlicedCachedFileDetails]))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[AutomaticChecksStubService].toInstance(mockService))
+        .build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ResultOfAutomaticChecksController.onPageLoad().url)
+        val result  = route(application, request).value
+
+        status(result)                 mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery when the service returns an error" in {
+
+      when(mockService.getSubmissions(any()))
+        .thenReturn(ResultT.fromError[Seq[SlicedCachedFileDetails]](InternalServerError))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[AutomaticChecksStubService].toInstance(mockService))
+        .build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.ResultOfAutomaticChecksController.onPageLoad().url)
+        val result  = route(application, request).value
+
+        status(result)                 mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must render submissions with the most recent at the top" in {
+
+      val unsorted = Seq(
+        ResultOfAutomaticChecksStubData.unprocessableErrorFile,
+        ResultOfAutomaticChecksStubData.virusFoundFile,
+        ResultOfAutomaticChecksStubData.passedFile
+      )
+
+      when(mockService.getSubmissions(any())).thenReturn(ResultT.fromValue(unsorted))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
+        .overrides(bind[AutomaticChecksStubService].toInstance(mockService))
+        .build()
+
+      running(application) {
+        val appConfig = application.injector.instanceOf[FrontendAppConfig]
+        val request   = FakeRequest(GET, routes.ResultOfAutomaticChecksController.onPageLoad().url)
+
+        val result = route(application, request).value
+        val view   = application.injector.instanceOf[ResultOfAutomaticChecksView]
+
+        val expectedSorted = unsorted.sortBy(_.dateSubmitted)(Ordering[LocalDateTime].reverse)
+
+        status(result)          mustEqual OK
+        contentAsString(result) mustEqual
+          view(expectedSorted, appConfig.managementUrl)(request, messages(application)).toString
+      }
+    }
+  }
+}
