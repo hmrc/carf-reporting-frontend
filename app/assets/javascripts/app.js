1ac8a849d3d73b390925aa7f6fd19f53ab21ed9b
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2026-09-23 11:19:34 +0100

    CARF-688 Back link fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,16 +5,6 @@ if (window.history && window.history.replaceState && typeof window.history.repla
 
 document.addEventListener('DOMContentLoaded', function (event) {
 
-    // handle back click
-    var backLink = document.querySelector('.govuk-back-link');
-    if (backLink !== null) {
-        backLink.addEventListener('click', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            window.history.back();
-        });
-    }
-
     // handle print click
     var printLink = document.querySelector('.print-link');
     if (printLink !== null) {
